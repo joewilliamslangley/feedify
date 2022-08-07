b679e88172e19c790dbffde250b9d1085cfb3d40
Joe Williams-Langley <joewilliamslangley@gmail.com>
2022-08-07 13:10:05 +0100

    created skeleton for feedify app

---
+++ server/server.js
@@ -0,0 +1,36 @@
+require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const SpotifyWebApi = require("spotify-web-api-node")
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post("/login", (req, res) => {
+  const code = req.body.code
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: 'http://localhost:3000',
+    clientId: '0ab62b72024b4e79b14900920249deea',
+    clientSecret: '894966344f6d4aba86c67d85a5008b1e'
+  })
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then(data => {
+      console.log('Auth successful')
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.sendStatus(400)
+    })
+})
+
+app.listen(3001)
