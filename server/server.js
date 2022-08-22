c713e4901a91ffaa0b2daf89d696d73159a35ebb
Joe Williams-Langley <joewilliamslangley@gmail.com>
2022-08-22 19:11:49 +0100

    restructured folders for heroku

--- server/server.js
+++ server/server.js
@@ -9,7 +9,11 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
 
 app.post("/login", (req, res) => {
   const code = req.body.code
