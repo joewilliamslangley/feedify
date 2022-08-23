66c15deac002d441dcd1ecc3f5e00f1e7ff4d2b1
Joe Williams-Langley <joewilliamslangley@gmail.com>
2022-08-23 15:36:29 +0100

    fixed static path string

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, "/client")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
