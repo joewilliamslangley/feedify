a60c5b011fb4a9f3bf7f339e5083c877b8c5e609
Joe Williams-Langley <joewilliamslangley@gmail.com>
2022-08-26 09:41:49 +0100

    checks to see end point in deployment

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -4,8 +4,10 @@ import BrandBar from "./BrandBar"
 import './App.css'
 
 const code = new URLSearchParams(window.location.search).get("code")
+const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT || 'http://localhost:3000';
 
 function App() {
+  console.log(API_ENDPOINT)
   return (
     <div className="app">
       <BrandBar />
